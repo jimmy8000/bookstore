e81c5c78dbb498064d230e600f00ac90fdb2ff4b
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-05-29 11:41:44 -0500

    Update ALLOWED_HOSTS with site and development server URL

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['obscure-gorge-90883-d72f1df05288.herokuapp.com']
 
 
 # Application definition
